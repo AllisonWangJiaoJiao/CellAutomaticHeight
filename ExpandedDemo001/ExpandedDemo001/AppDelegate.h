1de81d0a825e0f73c9a69f0ff856103839d53ff0
AllisonWangJiaoJiao <alliosnwangjj@163.com>
2017-02-28 22:48:42 +0800

    ExpandedDemo001

---
+++ ExpandedDemo001/ExpandedDemo001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandedDemo001
+//
+//  Created by Allison on 2017/2/28.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpandedDemo001/ExpandedDemo001/Contorller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExpandedDemo001
+//
+//  Created by Allison on 2017/2/28.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
